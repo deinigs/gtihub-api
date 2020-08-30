9fa581e77c78decced33f51c88596f23d01f5fd7
Danny Godoy <dangodoy84@gmail.com>
2020-08-29 21:13:25 -0400

    Added Skeleton Modules, Components and Services

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DefaultComponent } from './layout/default/default.component';
+import { UserSelectorComponent } from './routes/user-selector/user-selector.component';
+import { ProfileComponent } from './routes/profile/profile.component';
+import { RepoComponent } from './routes/repo/repo.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '', component: DefaultComponent,
+  children: [{
+    path: '',
+    component: UserSelectorComponent
+  }, {
+    path: 'profile',
+    component: ProfileComponent
+  }, {
+    path: 'repo',
+    component: RepoComponent
+  }]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
